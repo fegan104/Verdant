8a85c1fd2e699f76f142926df1c07c8cedbe042f
fegan104 <fegan104@gmail.com>
2025-09-21 19:44:32 -0400

    mostly working download button

--- app/src/main/java/com/frankegan/verdant/feature/imagedetail/ImageDetailScreen.kt
+++ app/src/main/java/com/frankegan/verdant/feature/imagedetail/ImageDetailScreen.kt
@@ -33,16 +33,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.Manifest
+import android.R.attr.onClick
+import android.content.pm.PackageManager
+import android.os.Build
+import android.system.Os
+import android.system.Os.link
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
 import com.frankegan.verdant.feature.viewer.ImageViewerRoute
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import java.text.NumberFormat
+import androidx.core.net.toUri
 
 @Serializable
 data class ImageDetailRoute(val imageId: String, val link: String)
@@ -59,6 +81,21 @@ fun ImageDetailScreen(
     val viewModel = viewModel<ImageDetailViewModel>(factory = ImageDetailViewModel.Factory(imageId))
     val imageResult by viewModel.imgurImage.collectAsStateWithLifecycle(null)
     val image = imageResult?.getOrNull()
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            downloadImage(
+                context = context,
+                url = link,
+                title = imageId,
+                description = image?.description.orEmpty()
+            )
+        }
+    }
 
     Scaffold(contentWindowInsets = WindowInsets(0)) { innerPadding ->
         Column(
@@ -113,6 +150,24 @@ fun ImageDetailScreen(
                     icon = Icons.Default.Download,
                     contentDescription = "down arrow",
                     text = "Download",
+                    onClick = {
+                        scope.launch {
+                            when {
+                                needsStoragePermission(context) -> {
+                                    launcher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                }
+
+                                else -> {
+                                    downloadImage(
+                                        context = context,
+                                        url = link,
+                                        title = image?.title.orEmpty(),
+                                        description = image?.description.orEmpty()
+                                    )
+                                }
+                            }
+                        }
+                    }
                 )
                 ImageDetail(
                     icon = Icons.Default.Share,
@@ -125,9 +180,45 @@ fun ImageDetailScreen(
 }
 
 @Composable
-private fun ImageDetail(icon: ImageVector, contentDescription: String, text: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(8.dp)) {
+private fun ImageDetail(
+    icon: ImageVector,
+    contentDescription: String,
+    text: String,
+    onClick: () -> Unit = {},
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = ripple(
+                radius = Dp.Unspecified,
+                bounded = false
+            ),
+            onClick = onClick,
+        ),
+    ) {
         Icon(icon, contentDescription)
         Text(text, style = MaterialTheme.typography.titleSmall)
     }
+}
+
+private fun needsStoragePermission(context: Context): Boolean {
+    val permissionCheck = ContextCompat.checkSelfPermission(
+        context,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    return Build.VERSION.SDK_INT < Build.VERSION_CODES.Q
+            && (permissionCheck == PackageManager.PERMISSION_DENIED)
+}
+
+private fun downloadImage(context: Context, url: String, title: String, description: String) {
+    val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+    val request = DownloadManager.Request(url.toUri())
+        .setTitle(title)
+        .setDescription(description)
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, title)
+    downloadManager.enqueue(request)
 }
